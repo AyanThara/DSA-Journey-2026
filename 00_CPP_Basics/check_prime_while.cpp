2ef0c7501e17cc7dc9f5cd0527d6486339f06d78
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-05 03:25:35 -0800

    Add prime number checking program in C++

---
+++ 00_CPP_Basics/check_prime_while.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int i = 2;
+    bool isPrime = true;
+
+    while (i < n) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+        i++;
+    }
+    if (isPrime)
+        cout << "Prime" << endl;
+    else
+        cout << "Not prime" << endl;
+}
