85a78bd0d47863738cfcee52b402bc0a8ffb0aa6
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-05 03:30:24 -0800

    Add program to sum odd numbers up to n

---
+++ 00_CPP_Basics/sum_odd_numbers_while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int i = 1, sum = 0;
+    while (i <= n) {
+        sum += i;
+        i += 2;
+    }
+    cout << sum;
+}
