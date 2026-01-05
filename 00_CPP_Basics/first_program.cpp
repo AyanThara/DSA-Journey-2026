fc3545d15112181511eddba1f04289d4d3bec6db
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-05 01:56:33 -0800

    Add first C++ program that prints 'Hello, World!'

---
+++ 00_CPP_Basics/first_program.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// Day 1: First C++ program
+// Learned basics of C++, variables, and data types
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
