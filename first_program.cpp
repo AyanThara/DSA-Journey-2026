4c61862fa04089ee8247d9c68677d5847ee340ed
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-05 01:46:26 -0800

    Update Day 1 topics and notes in first_program.cpp
    Added topics and notes for Day 1 programming concepts.

--- first_program.cpp
+++ first_program.cpp
@@ -1,10 +1,14 @@
 ## 📅 Day 1
 
 ### Topics Covered
-- Flowcharts (basic logic building)
-- C++ basics:
-  - Data types
-  - Variables
+- Flowcharts 
+1 Triangle or not
+2 Sum of numbers
+3 Sum of numbers till n
+4 Factorial of number
+5 Prime numbers
+ 
+-pseudo code 
 
 ### Notes
 - Understood how flowcharts help before coding
