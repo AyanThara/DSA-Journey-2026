3aa74f205cd98ab885b786941852f35d8f6b9ea5
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-05 01:51:59 -0800

    Add Day 2 topics and notes to first_program.cpp

--- first_program.cpp
+++ first_program.cpp
@@ -1,5 +1,4 @@
 ## 📅 Day 1
-
 ### Topics Covered
 - Flowcharts 
 1 Triangle or not
@@ -7,9 +6,27 @@
 3 Sum of numbers till n
 4 Factorial of number
 5 Prime numbers
- 
 -pseudo code 
-
 ### Notes
 - Understood how flowcharts help before coding
 - Learned how variables store data in C++
+
+-------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+## 📅 Day 2
+### Topics Covered
+- C++ compilation process
+- Variable naming conventions
+- Storage of positive and negative numbers
+- Signed int vs unsigned int
+- Operators:
+  - Arithmetic
+  - Relational
+  - Logical
+- First C++ program
+### Notes
+- Understood how C++ source code is compiled step by step
+- Learned the difference between signed and unsigned integers
+- Practiced basic operators in C++
+-------------------------------------------------------------------------------------------------------------------------------------------------
+
