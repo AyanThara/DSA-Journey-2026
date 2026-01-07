a11d351732885fa44eba6d7c0e7ad1cd56c938e4
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-07 04:46:06 -0800

    Update 10pattern_alphabet_square.cpp

--- 00_CPP_Basics/10pattern_alphabet_square.cpp
+++ 00_CPP_Basics/10pattern_alphabet_square.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 // Alphabet square pattern
 // Example:
 // Input: 4
@@ -9,16 +8,13 @@ using namespace std;
 // BBBB
 // CCCC
 // DDDD
-
 int main() {
     int n;
     cin >> n;
-
     int row = 1;
     while (row <= n) {
         int col = 1;
-        char ch = 'A' + row - 1;   // character for the current row
-
+        char ch = 'A' + row - 1;    
         while (col <= n) {
             cout << ch;
             col++;
@@ -26,5 +22,4 @@ int main() {
         cout << endl;
         row++;
     }
-    return 0;
 }
