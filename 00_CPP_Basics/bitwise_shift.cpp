f78bee9461b83458e266b3658538a77a47ea2e81
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-08 08:56:09 -0800

    Create bitwise_shift.cpp

---
+++ 00_CPP_Basics/bitwise_shift.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    cout << "Left Shift (5 << 1): " << (a << 1) << endl;   // 5 * 2 = 10
+    cout << "Right Shift (5 >> 1): " << (a >> 1) << endl;  // 5 / 2 = 2
+}
