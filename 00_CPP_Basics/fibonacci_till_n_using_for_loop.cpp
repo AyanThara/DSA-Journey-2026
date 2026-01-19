6d354feadd2cf5f8b20f92891d810834354acd32
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-01-19 09:36:30 -0800

    Create fibonacci_till_n_using_for_loop.cpp

---
+++ 00_CPP_Basics/fibonacci_till_n_using_for_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+// Program to print Fibonacci series till N terms using for loop
+int main() {
+    int n;
+    cin >> n;
+
+    int a = 0, b = 1;
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+}
